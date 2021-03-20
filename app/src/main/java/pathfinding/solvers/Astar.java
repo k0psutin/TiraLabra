17e8f5cf558a226b0c67354cece319f0a632dfbe
k0psutin <mr.gravez@gmail.com>
2021-03-20 18:39:45 +0200

    Clean unused functions

--- app/src/main/java/pathfinding/solvers/Astar.java
+++ app/src/main/java/pathfinding/solvers/Astar.java
@@ -14,18 +14,12 @@ public class Astar {
   private PriorityQueue<Node> open;
   private BufferedImage map;
 
-  private int width;
-  private int height;
-
   private int endX;
   private int endY;
   public boolean hasSolution = false;
 
   public Astar(BufferedImage map) {
-    this.width = map.getWidth();
-    this.height = map.getHeight();
     this.map = map;
-
     this.closed = new PriorityQueue<>();
     this.open = new PriorityQueue<>();
   }
@@ -63,21 +57,17 @@ public class Astar {
     return Duration.between(start, end).toMillis();
   }
 
-  public boolean isBorder(int x, int y) {
-    return (x < 0 || x > this.width - 1 || y < 0 || y > this.height - 1);
-  }
-
-  public boolean isEligibleMove(int x, int y) {
+  private boolean isEligibleMove(int x, int y) {
     boolean goodMove = getPixelColor(x, y, map).equals("(229,229,229)");
     setPixelColor(x, y, 0, 255, 255, this.map);
     return goodMove;
   }
 
-  public int distance(int currentX, int currentY) {
+  private int distance(int currentX, int currentY) {
     return Math.abs(currentX - endX) + Math.abs(currentY - endY);
   }
 
-  public void addNeighbours(Node current) {
+  private void addNeighbours(Node current) {
     for (int x = -1; x <= 1; x++) {
       for (int y = -1; y <= 1; y++) {
         int newX = current.posX + x;
