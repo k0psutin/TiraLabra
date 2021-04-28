5e927eb3279298e4c7655dcb768c7c8dd86b4062
k0psutin <mr.gravez@gmail.com>
2021-04-28 20:24:38 +0300

    Update UI

--- app/src/main/java/pathfinding/ui/Gui.java
+++ app/src/main/java/pathfinding/ui/Gui.java
@@ -1,9 +1,11 @@
 package pathfinding.ui;
 
+import static pathfinding.tools.ImgTools.drawCircle;
 import static pathfinding.tools.ImgTools.loadImage;
 import static pathfinding.tools.ImgTools.resizeImage;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
@@ -17,7 +19,9 @@ import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.ProgressMonitor;
 import javax.swing.WindowConstants;
 import javax.swing.event.MouseInputAdapter;
 import pathfinding.data.PerformanceTests;
@@ -41,10 +45,10 @@ public class Gui implements Runnable {
 
   private static boolean addStart = false;
   private static boolean addEnd = false;
-  private static int startX = 10;
-  private static int startY = 10;
-  private static int endY = 100;
-  private static int endX = 100;
+  private static int startX = 100;
+  private static int startY = 100;
+  private static int endY = 700;
+  private static int endX = 700;
 
   private String[] paths = {"A*", "JPS", "IDA*"};
 
@@ -74,6 +78,8 @@ public class Gui implements Runnable {
               File selectedFile = fileChooser.getSelectedFile();
               file = selectedFile.getName();
               buffImg = resizeImage(panelImgSize, panelImgSize, loadImage(file, imgSize));
+              drawCircle(endX, endY, Color.RED, buffImg);
+              drawCircle(startX, startY, Color.GREEN, buffImg);
               imageIcon.setImage(buffImg);
               picPanel.repaint();
             }
@@ -109,6 +115,7 @@ public class Gui implements Runnable {
           @Override
           public void actionPerformed(ActionEvent e) {
             Pathfinding solve = null;
+            buffImg = loadImage(file, imgSize);
 
             String type = comboBox.getSelectedItem().toString();
             switch (type) {
@@ -137,6 +144,8 @@ public class Gui implements Runnable {
             if (newMap == null) {
               return;
             }
+            drawCircle(endX, endY, Color.RED, newMap);
+            drawCircle(startX, startY, Color.GREEN, newMap);
             imageIcon.setImage(resizeImage(panelImgSize, panelImgSize, newMap));
             picPanel.repaint();
           }
@@ -152,22 +161,41 @@ public class Gui implements Runnable {
           @Override
           public void actionPerformed(ActionEvent e) {
             buffImg = resizeImage(panelImgSize, panelImgSize, buffImg);
+            drawCircle(endX, endY, Color.RED, buffImg);
+            drawCircle(startX, startY, Color.GREEN, buffImg);
             imageIcon.setImage(buffImg);
             picPanel.repaint();
           }
         });
 
-    JButton runTests = new JButton("Run tests");
+    JButton runTests = new JButton("Run performance tests");
     panel.add(runTests);
     runTests.addActionListener(
         new ActionListener() {
           @Override
           public void actionPerformed(ActionEvent e) {
-            PerformanceTests tests = new PerformanceTests();
-            tests.runTests();
+            int choice = JOptionPane.showConfirmDialog(frame, "Run performance tests.");
+            if (choice == JOptionPane.YES_OPTION) {
+              new Thread(
+                      () -> {
+                        ProgressMonitor pm =
+                            new ProgressMonitor(frame, "Performance tests", "", 0, 27);
+                        pm.setMillisToDecideToPopup(0);
+                        PerformanceTests tests = new PerformanceTests();
+                        tests.runTests(pm);
+                        if (!pm.isCanceled()) {
+                          JOptionPane.showMessageDialog(
+                              frame, "Performance tests completed. See results.md at base dir.");
+                        } else {
+                          JOptionPane.showMessageDialog(frame, "Performance tests were cancelled.");
+                        }
+                      })
+                  .start();
+            }
           }
         });
-
+    drawCircle(startX, startY, Color.GREEN, buffImg);
+    drawCircle(endX, endY, Color.RED, buffImg);
     imageIcon = new ImageIcon(resizeImage(panelImgSize, panelImgSize, buffImg));
     picLabel = new JLabel(imageIcon);
     picLabel.setLayout(null);
@@ -183,21 +211,46 @@ public class Gui implements Runnable {
     frame.add(panel);
 
     picPanel = new JPanel();
+
     picLabel.addMouseListener(
         new MouseInputAdapter() {
           @Override
           public void mouseReleased(MouseEvent e) {
             Point pos = e.getPoint();
+            int newX = (int) (pos.x / factor);
+            int newY = (int) (pos.y / factor);
+
             if (Gui.addEnd) {
-              endX = (int) (pos.x / factor);
-              endY = (int) (pos.y / factor);
+              drawCircle(endX, endY, new Color(229, 229, 229), buffImg);
+              endX = newX;
+              endY = newY;
               addEnd = false;
               endPos.setText("End point: (" + endX + "," + endY + ")");
+              drawCircle(endX, endY, Color.RED, buffImg);
             } else if (Gui.addStart) {
-              startX = (int) (pos.x / factor);
-              startY = (int) (pos.y / factor);
+              drawCircle(startX, startY, new Color(229, 229, 229), buffImg);
+              startX = newX;
+              startY = newY;
               addStart = false;
               startPos.setText("Start point: (" + startX + "," + startY + ")");
+              drawCircle(startX, startY, Color.GREEN, buffImg);
+            }
+            imageIcon.setImage(buffImg);
+            picPanel.repaint();
+          }
+        });
+
+    picLabel.addMouseMotionListener(
+        new MouseInputAdapter() {
+          @Override
+          public void mouseMoved(MouseEvent e) {
+            Point pos = e.getPoint();
+            int newX = (int) (pos.x / factor);
+            int newY = (int) (pos.y / factor);
+            if (Gui.addEnd) {
+              endPos.setText("End point: (" + newX + "," + newY + ")");
+            } else if (Gui.addStart) {
+              startPos.setText("Start point: (" + newX + "," + newY + ")");
             }
           }
         });
